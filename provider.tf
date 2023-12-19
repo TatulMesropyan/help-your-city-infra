0e3b7d864f11caa5cd331fcfeddfc8f62466ee5a
t.mesropyan <tatul.mesropyan-extern@invia.de>
2023-12-20 01:45:36 +0400

    Remove

--- provider.tf
+++ provider.tf
@@ -2,7 +2,5 @@ provider "aws" {
   # region     = var.region
   # profile = var.aws_config_profile
   region = "eu-central-1"
-  # secret_key = "ac7X+xcpKmXoeUeuIuWr8eNSkwZZM+cKTazjRokO"
-  # access_key = "AKIAR7LQWUWTCPVMWO7E"
   # shared_credentials_files = [var.aws_credential_file]
 }
